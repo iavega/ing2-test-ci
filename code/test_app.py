b92cef92540e7e9dcf3e903f520f14d15190bf13
Iam Vega <iam.vega@intechideas.com>
2022-07-04 18:41:59 -0500

    update template and test1

--- code/test_app.py
+++ code/test_app.py
@@ -8,5 +8,5 @@ def test_success_request_page_route(app, client):
 
 def test_render_html_page_route(app, client):
     res = client.get('/')
-    expected = '<h2 class="text-center">Flask Example CI/CD</h2>'
+    expected = '<h2 class="text-center">Flask Example CI/CD001</h2>'
     assert expected in res.get_data(as_text=True)
